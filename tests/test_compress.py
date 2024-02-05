826f2a31fe87a1515525f981cdb6f5a5f295d2e0
Watanabe Takashi <wtnb75@gmail.com>
2024-02-05 23:10:21 +0900

    unittest

--- tests/test_compress.py
+++ tests/test_compress.py
@@ -1,5 +1,8 @@
 import unittest
-from log2s3.compr import extcmp_map
+import pathlib
+import tempfile
+import subprocess
+from log2s3.compr import extcmp_map, auto_compress, do_chain
 
 
 class TestCompr(unittest.TestCase):
@@ -10,3 +13,16 @@ class TestCompr(unittest.TestCase):
                 compdata = v[2](input_bytes)
                 restored = v[1](compdata)
                 self.assertEqual(input_bytes, restored)
+
+    def test_autocomp(self):
+        input_bytes = b'abcdefg1234567' * 100
+        with tempfile.TemporaryDirectory() as td:
+            tf1 = pathlib.Path(td, "example.data")
+            tf2 = pathlib.Path(td, "example.data.gz")
+            tf1.write_bytes(input_bytes)
+            res = subprocess.run(["gzip", "-f", tf1])
+            self.assertEqual(0, res.returncode)
+            self.assertTrue(tf2.exists())
+            p1, ls = auto_compress(tf2, "decompress")
+            self.assertEqual("example.data", pathlib.Path(p1).name)
+            self.assertEqual(input_bytes, do_chain(ls))
---
+++ tests/test_processor.py
@@ -0,0 +1,56 @@
+import unittest
+from datetime import datetime, timedelta
+import tempfile
+import pathlib
+import os
+from log2s3.processor import process_walk, DelProcessor, CompressProcessor
+
+
+class TestProcessor(unittest.TestCase):
+    def setUp(self):
+        self.td = tempfile.TemporaryDirectory()
+        self.basedir = pathlib.Path(self.td.name)
+        for dir in [f"dir{x}" for x in range(5)]:
+            (self.basedir / dir).mkdir()
+            for day in range(10):
+                dt = datetime.now()-timedelta(days=day)
+                sz = (day+1) * 1024
+                logfile = (self.basedir / dir / dt.strftime("%Y-%m-%d.log"))
+                logfile.write_bytes(b'\0'*sz)
+                os.utime(logfile, (dt.timestamp(), dt.timestamp()))
+
+    def tearDown(self):
+        del self.td
+
+    def _count(self) -> tuple[int, int]:
+        cnt = 0
+        sz = 0
+        for root, _, files in os.walk(self.basedir):
+            cnt += len(files)
+            sz += sum([pathlib.Path(root, x).stat().st_size for x in files])
+        return cnt, sz
+
+    def test_delete1(self):
+        dp = DelProcessor({"older": "2d", "bigger": "1k"})
+        process_walk(self.basedir, [dp])
+        self.assertEqual(10, self._count()[0])  # 2 files x 5 dirs
+
+    def test_delete2(self):
+        dp = DelProcessor({"older": "1d", "bigger": "4.1k"})
+        process_walk(self.basedir, [dp])
+        self.assertEqual(20, self._count()[0])  # 4 files x 5 dirs
+
+    def test_compress1(self):
+        pre_cnts = self._count()
+        dp = CompressProcessor({"older": "2d", "bigger": "1k", "compress": "gzip"})
+        process_walk(self.basedir, [dp])
+        cnts = self._count()
+        self.assertEqual(pre_cnts[0], cnts[0])
+        self.assertGreater(pre_cnts[1], cnts[1])
+
+    def test_compress2(self):
+        pre_cnts = self._count()
+        dp = CompressProcessor({"bigger": "100k", "compress": "gzip"})
+        process_walk(self.basedir, [dp])
+        cnts = self._count()
+        self.assertEqual(pre_cnts, cnts)  # do nothing
